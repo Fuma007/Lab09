f88483f1995ddd14808addd8808a22c7210805f1
Fuma007 <techit_wongkiti@cmu.ac.th>
2026-09-21 16:02:05 +0700

    Lab09-3.5 commit*

--- src/main/java/se331/lab09/controller/EventController.java
+++ src/main/java/se331/lab09/controller/EventController.java
@@ -1,39 +1,36 @@
 package se331.lab09.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
-import org.springframework.data.domain.Page;
 import se331.lab09.entity.Event;
 import se331.lab09.service.EventService;
+import se331.lab09.util.LabMapper;
 
 @RestController
 @RequiredArgsConstructor
 public class EventController {
-
     final EventService eventService;
 
     @GetMapping("/events")
     public ResponseEntity<?> getEventLists(@RequestParam(value = "_limit", required = false) Integer perPage,
                                            @RequestParam(value = "_page", required = false) Integer page) {
         Page<Event> pageOutput = eventService.getEvents(perPage, page);
-        HttpHeaders responseHeaders = new HttpHeaders();
-        responseHeaders.set("x-total-count", String.valueOf(pageOutput.getTotalElements()));
-        try {
-            return ResponseEntity.ok().headers(responseHeaders).body(pageOutput.getContent());
-        } catch (IndexOutOfBoundsException e) {
-            return ResponseEntity.ok().headers(responseHeaders).body(pageOutput.getContent());
-        }
+        HttpHeaders responseHeader = new HttpHeaders();
+        responseHeader.set("x-total-count", String.valueOf(pageOutput.getTotalElements()));
+        return new ResponseEntity<>(LabMapper.INSTANCE.getEventDto(pageOutput.getContent()),
+                responseHeader, HttpStatus.OK);
     }
 
-    @GetMapping("events/{id}")
+    @GetMapping("/events/{id}")
     public ResponseEntity<?> getEvent(@PathVariable("id") Long id) {
         Event output = eventService.getEvent(id);
         if (output != null) {
-            return ResponseEntity.ok(output);
+            return ResponseEntity.ok(LabMapper.INSTANCE.getEventDto(output));
         } else {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "The given id is not found");
         }
@@ -42,6 +39,6 @@ public class EventController {
     @PostMapping("/events")
     public ResponseEntity<?> addEvent(@RequestBody Event event) {
         Event output = eventService.save(event);
-        return ResponseEntity.ok(output);
+        return ResponseEntity.ok(LabMapper.INSTANCE.getEventDto(output));
     }
 }
